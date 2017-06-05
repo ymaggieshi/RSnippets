b603cf749e72c8cc73d50a4dda4b0ef9956c8ecb
ymaggieshi <yu.shi.stat@gmail.com>
2017-06-05 00:19:22 -0700

    Add files via upload

---
+++ load_multiple_libraries.R
@@ -0,0 +1,6 @@
+## install and load multiple libraries at once
+
+load.libraries <- c('data.table', 'testthat', 'gridExtra', 'corrplot', 'GGally', 'ggplot2', 'e1071', 'dplyr')
+install.lib <- load.libraries[!load.libraries %in% installed.packages()]
+for(libs in install.lib) install.packages(libs, dependences = TRUE)
+sapply(load.libraries, require, character = TRUE)
